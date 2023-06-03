b333737b0c43049985d594e89061da7997677e72
Xannetz Suficiencia <xvsuficiencia@gmail.com>
2023-06-03 11:13:23 +0800

    Created the Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Landing from './pages/landingPage';
+import SignUp from './pages/registrationPage';
+import LogIn from './pages/loginPage';
+import Home from './pages/homePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
